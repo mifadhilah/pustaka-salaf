f6cd91e246fd2b1bfec530bc2f37ddaf076164a4
mifadhilah <irfanfadh.dev@gmail.com>
2025-07-20 11:25:06 +0700

    initiate client side

---
+++ client/src/app/daftar-buku/page.tsx
@@ -0,0 +1,50 @@
+
+export default function Books() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      Halaman Daftar buku
+
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu ante ac dui pretium
+      pulvinar non ut neque. Donec at elementum nisl, posuere accumsan libero. Pellentesque
+      tempus dolor sed ligula porta tempus. Vivamus mattis velit ac eros viverra, sit amet
+      bibendum sapien mollis. Sed suscipit venenatis mollis. Pellentesque non arcu euismod,
+      aliquam nisl varius, molestie arcu. Etiam leo lorem, faucibus vitae viverra viverra,
+      dignissim et ex. Vivamus diam nunc, consectetur ullamcorper elit et, ullamcorper pulvinar
+      mauris. Nam enim sem, tempus ut porta at, tempor a libero. Morbi et ipsum nec ex consequat
+      euismod vulputate et metus. Donec ut ultrices arcu, sit amet condimentum purus. Curabitur
+      nec neque accumsan, viverra justo quis, semper urna. Nam tortor felis, tincidunt sit amet
+      turpis ac, semper elementum leo. Pellentesque habitant morbi tristique senectus et netus et
+      malesuada fames ac turpis egestas. Fusce egestas leo lorem, eget semper lorem auctor ut.
+      Suspendisse et vehicula sapien, quis finibus felis. Nam vel vulputate elit, vel euismod
+      dui. Donec nec velit ut diam hendrerit vestibulum sed et arcu. Morbi ac mi tempus, bibendum
+      nulla euismod, rutrum dui. Donec cursus diam libero, ac faucibus justo porta eu. Suspendisse
+      elementum accumsan nunc, et ultrices risus malesuada sed. Cras ac eros leo.
+
+      Mauris libero magna, dapibus ut sagittis non, vehicula vitae diam. Vivamus tempor erat elit,
+      nec tincidunt ipsum elementum eu. Etiam dolor purus, commodo sed urna id, pulvinar iaculis
+      orci. Curabitur eu scelerisque magna, nec imperdiet turpis. Aenean tellus magna, fermentum
+      ac urna vitae, varius lobortis magna. Quisque laoreet nibh nisl, mattis consectetur arcu
+      interdum ut. Pellentesque porttitor tempor urna non malesuada. Fusce auctor vitae nibh
+      accumsan vestibulum. Duis facilisis tincidunt arcu non faucibus. Mauris sed urna lobortis
+      eros tincidunt pharetra quis ac odio. Nunc convallis mattis sem, a pellentesque tortor
+      hendrerit non. Nunc interdum, ante sit amet viverra mattis, ipsum purus pharetra augue,
+      sit amet aliquam elit orci nec leo. Duis vestibulum ipsum eu rhoncus mollis. Fusce ligula
+      mauris, sagittis non mi quis, facilisis efficitur leo. Integer nec mauris id elit gravida
+      accumsan ac quis sapien. Ut mollis efficitur metus eu maximus.
+
+      Nam non commodo turpis. Quisque a faucibus dui, id lobortis urna. Duis egestas purus
+      hendrerit nulla ultricies euismod. In id metus vehicula, scelerisque urna eleifend,
+      auctor turpis. Cras nibh est, convallis nec tellus vitae, bibendum porttitor sapien.
+      In fringilla sapien vitae posuere congue. Orci varius natoque penatibus et magnis dis
+      parturient montes, nascetur ridiculus mus. Suspendisse elit nunc, imperdiet et lectus
+      at, eleifend dignissim metus. Etiam suscipit semper fringilla. Pellentesque habitant
+      morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+      Quisque blandit cursus dolor sed porta. Suspendisse eget risus ut risus sollicitudin tempor
+      et vel lorem. Quisque pretium tortor at dolor efficitur sollicitudin. In hendrerit sodales
+      ipsum eu rutrum. Donec tincidunt urna sit amet arcu rutrum, at bibendum velit laoreet. Etiam
+      non massa ac nisi varius lacinia. Sed pretium nulla ut ornare hendrerit. Sed pretium
+      fringilla sapien, vel viverra mi laoreet sit amet.
+    </div>
+  );
+}
